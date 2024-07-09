3a2d8b002bf07c305e6a8b664d38051873e827b7
Daniel <daniel.paschoalino@aluno.faculdadeimpacta.com.br>
2024-07-09 08:28:06 -0300

    Commit addEnvironment Compatibility

--- utils/selenium_utils.py
+++ utils/selenium_utils.py
@@ -43,10 +43,12 @@ def search_in_browser(browser: Selenium, search_input: str, section: str) -> Non
     
     browser.wait_until_element_is_visible('//button[@aria-label="Open search bar"]', timeout=10)
     browser.click_element('//button[@aria-label="Open search bar"]')
+    try:
+        browser.wait_until_element_is_visible('//*[@id="onetrust-reject-all-handler"]', timeout=10)
+        browser.click_element('//*[@id="onetrust-reject-all-handler"]')
+    except:
+        pass
 
-    browser.wait_until_element_is_visible('//*[@id="onetrust-reject-all-handler"]', timeout=10)
-    browser.click_element('//*[@id="onetrust-reject-all-handler"]')
-    
     browser.wait_until_element_is_visible('//input[@type="search"]', timeout=10)
     browser.input_text('//input[@type="search"]', search_input)
     
